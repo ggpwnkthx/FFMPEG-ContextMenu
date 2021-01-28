1a36b71c61dfdeb99018fb015a0923d887f6802c
ggpwnkthx <ibjessup@gmail.com>
2021-01-28 11:34:47 -0500

    Update convert_all.ps1
    Normalized output filenames.
    Fixed registry paths.

--- convert_all.ps1
+++ convert_all.ps1
@@ -4,10 +4,10 @@ $defaults = @{
 	'c:a'			= 'aac';
 	'b:a'			= '317k';
 	'aspect:v'		= '2';
-	'movflags'      = '+faststart';
+    'movflags'      = '+faststart';
 };
 $outputs = @{
-    '960p' = @{
+    '1k' = @{
 	    'c:v'	   = 'libx264';
 	    'crf'      = '23';
         'coder'    = '0'
@@ -271,18 +271,24 @@ if ($PSScriptRoot -ne "$dir_scope\Scripts") {
 
     # Context menu
     $reg_context = "HKCU:\SOFTWARE\Classes\Directory\Background\shell"
+    if(!(Test-Path $reg_context)) {
+        New-Item -Path $reg_context
+    }
     $reg_context_ffmpeg = "$reg_context\ffmpeg"
-    $reg_context_ffmpeg_shell = "$reg_context_ffmpeg\shell"
-    $reg_context_ffmpeg_shell_all = "$reg_context_ffmpeg_shell\all"
-    $reg_context_ffmpeg_shell_all_command = "$reg_context_ffmpeg_shell_all\command"
     if(!(Test-Path $reg_context_ffmpeg)) {
-        New-Item -Path $reg_context -Name "ffmpeg" | Out-Null
-        New-ItemProperty -Path $reg_context_ffmpeg -Name "SubCommands" -Value ""
-        New-ItemProperty -Path $reg_context_ffmpeg -Name "MUIVerb" -Value "FFMPEG"
-        New-Item -Path $reg_context_ffmpeg -Name "shell" | Out-Null
-        New-Item -Path $reg_context_ffmpeg_shell -Name "all" | Out-Null
-        New-ItemProperty -Path $reg_context_ffmpeg_shell_all -Name "MUIVerb" -Value "Convert All MP4s"
-        New-Item -Path $reg_context_ffmpeg_shell_all -Name "command" | Out-Null
+        New-Item -Path $reg_context_ffmpeg | Out-Null
+    }
+    $reg_context_ffmpeg_shell = "$reg_context_ffmpeg\shell"
+    if(!(Test-Path $reg_context_ffmpeg_shell)) {
+        New-Item -Path $reg_context_ffmpeg_shell | Out-Null
+    }
+    $reg_context_ffmpeg_shell_all = "$reg_context_ffmpeg_shell\all"
+    if(!(Test-Path $reg_context_ffmpeg_shell_all)) {
+        New-Item -Path $reg_context_ffmpeg_shell_all | Out-Null
+    }
+    $reg_context_ffmpeg_shell_all_command = "$reg_context_ffmpeg_shell_all\command"
+    if(!(Test-Path $reg_context_ffmpeg_shell_all_command)) {
+        New-Item -Path $reg_context_ffmpeg_shell_all_command | Out-Null
     }
     Set-ItemProperty -Path $reg_context_ffmpeg_shell_all_command -Name "(Default)" -Value "PowerShell.exe -ExecutionPolicy Bypass -NoExit -Command `"& '$self_install_path' '%v'`""
     Write-Host -NoNewLine 'Installed successfully! ';
